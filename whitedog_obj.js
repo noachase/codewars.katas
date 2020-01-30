2e1cf685302b96b65f04ca4aafc081bb96593f75
Panacea <noach4se@gmail.com>
2020-01-30 09:14:18 +0200

    added best practice for whitedog_kata

--- whitedog_obj.js
+++ whitedog_obj.js
@@ -1,7 +1,9 @@
 function animal(obj){
-    var color = obj.color;
-    var name = obj.name;
-    var legs = obj.legs;
+    let color = obj.color;
+    let name = obj.name;
+    let legs = obj.legs;
     
     return `This ${color} ${name} has ${legs} legs.`
 }
+
+// var animal = obj => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
